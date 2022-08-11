89c7ee0591b0022e778990c789f2a9407ddbc455
vishalraymal <vishalraymal@gmail.com>
2022-08-11 16:04:16 +0530

    Updated order feature as per review comment

--- src/Pages/OrderPage.java
+++ src/Pages/OrderPage.java
@@ -5,6 +5,7 @@ public class OrderPage {
 	public static void orderDo()
 	{
 		System.out.println("Order page");
+		System.out.println("LOGIC HERE ");
 	}
 
 }
--- src/Tests/OrderPageTest.java
+++ src/Tests/OrderPageTest.java
@@ -4,6 +4,7 @@ public class OrderPageTest {
 	
 	public void orderPageTests() {
 		System.out.println("True");
+		System.out.println("Added assertion");
 	}
 
 }
