36908e185851afca4cd1c9468c4a5452ea352291
vishalraymal <vishalraymal@gmail.com>
2022-08-12 23:59:13 +0530

    added classic pagewith name info

--- src/Pages/ClassicPage.java
+++ src/Pages/ClassicPage.java
@@ -1,14 +1,15 @@
 package Pages;
 
 public class ClassicPage {
-	
+
 	public void classic() {
 
-		int k=100;
-		int k1=200;
-		int div=k/k1;
+		int k = 100;
+		int k1 = 200;
+		int div = k / k1;
 		System.out.println(div);
-    System.out.println("bye here");
+		System.out.println("bye here");
+		System.out.println("Vishal R");
 	}
 
 }
