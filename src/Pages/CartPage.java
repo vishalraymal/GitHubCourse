563b2caebdcded09fd2e2bab677b5b5a91b8e727
vishalraymal <vishalraymal@gmail.com>
2022-08-12 13:11:16 +0530

    new cart mobile feature added

--- src/Pages/CartPage.java
+++ src/Pages/CartPage.java
@@ -4,6 +4,7 @@ public class CartPage {
 		public static void main(String[] args) {
 			System.out.println("Cart Page..");
 			System.out.println("Cart Page feature..");
+			System.out.println("Cart Mobile feature added..");
 
 		}
 }
